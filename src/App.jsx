fbead0a6d77ce9467a82f33d6ceb80cf9d42e7a4
rizwansikanderr786-dev <rizwansikanderr.786@gmail.com>
2025-09-10 15:56:00 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Magnates Agency Landing Page</h1>
+      <p>This is a placeholder. Full code integration goes here.</p>
+    </div>
+  );
+}
+
+export default App;
